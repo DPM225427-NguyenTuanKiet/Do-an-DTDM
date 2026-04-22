35196cb60b7368aad4e43e4ba9619d75c1515bd1
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2026-04-22 16:02:21 +0700

    Fix 404 CDN and Namespace

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8580e9c85b572cc755495abbc33aea732f0ec284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a82382dae439fedbc12e5ac0f7830bcc293871e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
